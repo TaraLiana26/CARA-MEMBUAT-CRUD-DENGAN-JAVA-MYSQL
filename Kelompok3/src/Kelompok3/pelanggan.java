78128559fffef364eaa96d1bb13a005ff646f677
TaraLiana26 <102141856+TaraLiana26@users.noreply.github.com>
2022-05-24 23:41:16 +0700

    Add files via upload

---
+++ Kelompok3/src/Kelompok3/pelanggan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kelompok3;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author HP
+ */
+class pelanggan {
+
+    static void setModel(DefaultTableModel Tara) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
